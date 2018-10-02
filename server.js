3c1daf56e3dea829497fdb32824f65cf0d74ebc5
Adam Husain <medaman@gmail.com>
2018-10-02 08:42:03 -0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+
+app.use(express.static('client/public'));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.resolve(__dirname, 'client', 'build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'));
+});
+app.listen(PORT, () => {
+  console.log(`Server on PORT: ${PORT}`);
+});
